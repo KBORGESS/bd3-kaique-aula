129c04519d9fd782d46a7a02084a2fa5010f75d3
KBORGES <kaiqueborgestmz@gmail.com>
2024-08-06 20:40:22 -0300

    criação do banco de dados/collection

---
+++ create-database.mongodb.js
@@ -0,0 +1,16 @@
+/* NOME DO BANCO DE DADOS */
+const database = 'BD3-KAIQUE-AULA';
+
+/* NOME DO BANCO DE DADOS */
+const collection = 'livraria';
+
+/*CRIAR OU ACESSAR O BANCO DE DADOS*/
+use(database);
+
+/* CRIAR UMA COLEÇÃO DE DADOS */
+db.createCollection(collection);
+
+
+
+
+
