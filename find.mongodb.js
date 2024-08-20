b465361aae02f38dd1205e6aaba4458acd56b468
KBORGES <kaiqueborgestmz@gmail.com>
2024-08-20 20:32:21 -0300

    aula20/08/24 inserts

---
+++ find.mongodb.js
@@ -0,0 +1,14 @@
+/* NOME DO BANCO DE DADOS */
+const database = 'BD3-KAIQUE-AULA';
+
+/* NOME DO BANCO DE DADOS */
+const collection = 'livraria';
+
+/*CRIAR OU ACESSAR O BANCO DE DADOS*/
+use(database);
+
+/* CRIAR UMA COLEÇÃO DE DADOS */
+db.createCollection(collection);
+
+/* SELECIONA TODOS OS REGISTROS DA COLEÇÃO: */
+db[collection].find()
